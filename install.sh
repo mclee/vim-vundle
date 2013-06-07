93182d63460641a182e857c2ec16c7d5a88f0e65
George Lee <mclee@foolme.net>
2013-06-07 22:37:21 +0800

    rewrite the install script

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+DEST="~/.vim"
+MACHINE=`uname`
 
-git clone https://github.com/mclee/vim-vundle.git ~/.vim
-cd ~/.vim; make install;
+git clone https://github.com/mclee/vim-vundle.git $DEST
+git clone https://github.com/gmarik/vundle.git $DEST/bundle/vundle
+ln -sf $DEST/vimrc ~/.vimrc
+vim -c "execute \"BundleInstall\" | q | q"
+
+cd $DEST/bundle/Command-T/ruby/command-t; ruby extconf.rb; make clean && make
