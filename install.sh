c38169eab368d6e4dad67d95526835007923fa93
George Lee <mclee@foolme.net>
2015-05-17 22:29:24 +0800

    Remove command-t related stuff in install

--- install.sh
+++ install.sh
@@ -5,6 +5,4 @@ MACHINE=`uname`
 git clone https://github.com/mclee/vim-vundle.git $DEST
 git clone https://github.com/gmarik/Vundle.vim.git $DEST/bundle/Vundle.vim
 ln -sf $DEST/vimrc ~/.vimrc
-vim -c "execute \"BundleInstall\" | q | q"
-
-cd $DEST/bundle/Command-T/ruby/command-t; ruby extconf.rb; make clean && make
+vim +PluginInstall +qall
