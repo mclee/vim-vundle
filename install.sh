159b0e704394305a6021f631a761ed51b16dd76d
George Lee <glee@zendesk.com>
2018-10-01 16:09:14 +0800

    fix vundle new repo url

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ DEST="$HOME/.vim"
 MACHINE=`uname`
 
 git clone https://github.com/mclee/vim-vundle.git $DEST
-git clone https://github.com/gmarik/Vundle.vim.git $DEST/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git $DEST/bundle/Vundle.vim
 ln -sf $DEST/vimrc ~/.vimrc
 vim +PluginInstall +qall
