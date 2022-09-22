5425d82a472bde0a01b2b9e025764f68a2a31637
George Lee <mclee@foolme.net>
2022-09-22 21:08:41 +0800

    switch the install script for vim-plug

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ DEST="$HOME/.vim"
 MACHINE=`uname`
 
 git clone https://github.com/mclee/vim-vundle.git $DEST
-git clone https://github.com/VundleVim/Vundle.vim.git $DEST/bundle/Vundle.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -sf $DEST/vimrc ~/.vimrc
 vim +PluginInstall +qall
